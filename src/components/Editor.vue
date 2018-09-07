0d21b2adf9b7406ceb22eaa01425ee82a066e7e7
Anubhav <anubhavujjawal@gmail.com>
2018-09-07 13:07:53 +0530

    Toggle Editor bug

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="col-md-12 col-xs-12"><button @click="toggleEditor" class="btn btn-primary mb-5 mt-2 float-right">Toggle Editor</button></div><br>
-        <div v-bind:hidden="showEditor==true?true:null" class="Editor mx-auto col-md-10 col-xs-12 mt-5"/>
+        <div v-bind:hidden="showEditor===true?null:true" class="Editor mx-auto col-md-10 col-xs-12 mt-5"/>
     </div>
 </template>
 <script>
@@ -20,6 +20,7 @@ export default {
           if(this.$data.showEditor == true)
             this.$data.showEditor = false;
           else  this.$data.showEditor = true;
+          console.log(this.$data.showEditor);
       }
   },
   mounted: function(){
